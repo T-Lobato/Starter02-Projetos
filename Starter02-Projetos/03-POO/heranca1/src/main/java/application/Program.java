c6b2faff00b44aa50331051ebb12d17ba71100a8
T-Lobato <thyagollobato@gmail.com>
2022-07-17 14:44:48 -0300

    Entendendo o abstract

--- Starter02-Projetos/03-POO/heranca1/src/main/java/application/Program.java
+++ Starter02-Projetos/03-POO/heranca1/src/main/java/application/Program.java
@@ -4,6 +4,10 @@ import entities.Account;
 import entities.BusinessAccount;
 import entities.SavingsAccount;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class Program {
     public static void main(String[] args) {
 
@@ -57,16 +61,44 @@ public class Program {
 //        Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
 //        acc3.withDraw(200.0);
 //        System.out.println(acc3.getBalance());
+//
+//        // Entendendo Polimorfismo
+//        // Chamando a mesma operacao, em variaveis do mesmo tipo (x e y) obtendo resultados diferentes.
+//        Account x = new Account(1020, "Alex", 1000.0);
+//        Account y = new SavingsAccount(1023,"Maria", 1000.0, 0.01);
+//
+//        x.withDraw(50.0);
+//        y.withDraw(50.0);
+//
+//        System.out.println(x.getBalance());
+//        System.out.println(y.getBalance());
 
-        // Entendendo Polimorfismo
-        // Chamando a mesma operacao, em variaveis do mesmo tipo (x e y) obtendo resultados diferentes.
-        Account x = new Account(1020, "Alex", 1000.0);
-        Account y = new SavingsAccount(1023,"Maria", 1000.0, 0.01);
-
-        x.withDraw(50.0);
-        y.withDraw(50.0);
-
-        System.out.println(x.getBalance());
-        System.out.println(y.getBalance());
+//        //Entendendo o abstract
+//        // Usando o abstract na classe Account ela não pode mais ser instanciada.
+//        Account acc1 = new Account(1001, "Alex", 1000.0);
+//        Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
+//        Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
+//
+//        Locale.setDefault(Locale.US);
+//        List<Account> list = new ArrayList<>();
+//
+//        list.add(new SavingsAccount(1001, "Alex", 500.00, 0.01));
+//        list.add(new BusinessAccount(1002, "Maria", 1000.00, 400.00));
+//        list.add(new SavingsAccount(100, "Bob", 300.00, 0.01));
+//        list.add(new BusinessAccount(1005, "Anna", 500.0, 500.0));
+//
+//        double sum = 0.0;
+//        for(Account acc : list){
+//            sum += acc.getBalance();
+//        }
+//f
+//        System.out.printf("Total balance: %.2f\n", sum);
+//
+//        for(Account acc : list){
+//            acc.deposit(10.0);
+//        }
+//        for(Account acc : list){
+//            System.out.printf("Updated balance for account %d: %.2f\n", acc.getNumber(), acc.getBalance());
+//        }
     }
 }
